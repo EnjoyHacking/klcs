382c43a61c7e80c9e315d606e9e27bf14dd7d1c2
sang <sangyafei1026@gmail.com>
2015-08-26 15:52:10 +0000

    debug convertion

--- convertion.c
+++ convertion.c
@@ -1,11 +1,37 @@
 #include "lst_string.h"
 #include "convertion.h"
 
+#include <string.h>
+
+
+char * string_replace(const char *src, const char *pattern, const char *replacement) {
+
+	char * dst = (char *) malloc (sizeof(char) * strlen(src));
+
+	char * src_p = (char *)src;	
+
+	char * pos = NULL;
+
+	while ((pos = strstr(src_p, pattern)) != NULL)	{
+
+		size_t len = (size_t) (pos - src_p);
+		strncat(dst, src_p, len);	
+		strncat(dst, replacement, strlen(pattern));
+		src_p = pos + strlen(pattern);
+	}
+
+	strncat(dst, src_p, strlen(src) - (size_t)(src_p - src));
+
+	return dst;
+
+}
+/*
 char * string_replace(const char * src, char * dst, size_t dst_size, const char * search, const char * replace_with) {
 
 	char * replace_buf = (char *) malloc (dst_size);
 
 	if (replace_buf) {
+
 		replace_buf[0] = 0;
 		char * p = (char *) src;
 		char * pos = NULL;
@@ -28,11 +54,12 @@ char * string_replace(const char * src, char * dst, size_t dst_size, const char 
 
 	return dst;
 }
+*/
 
 
 void convert_token_cb(LST_String *token, void *data) {
 
-	convertiont_data_single_t * single = (convertion_data_single_t *) data;
+	convertion_data_single_t * single = (convertion_data_single_t *) data;
 
 	LST_String * flow = single->flow;
 	LST_String * flow_converted = single->flow_converted;
@@ -71,17 +98,17 @@ convertion_data_single_t * convertion_data_single_new() {
 
 convertion_data_set_t * convertion_data_set_new() {
 
-	convertion_data_set_t * data = (convertion_data_set_t *) malloc (sizeof(convertion_data_set));
+	convertion_data_set_t * data = (convertion_data_set_t *) malloc (sizeof(convertion_data_set_t));
 
 	data->flows_converted = lst_stringset_new();
 
-	data->tokens = tokens;
+	data->tokens = lst_stringset_new();
 
 	return data;
 }
 
 
-void convertion_data_set_free (convertion_data_set * data) {
+void convertion_data_set_free (convertion_data_set_t * data) {
 
 	if(!data) {
 		return;
@@ -113,23 +140,28 @@ int main(int argc, char **argv) {
 	lst_stringset_add(flows, flow2);
 
 
-	char * token1 = "HTTP1.1";
-	char * token2 = "HTTP1.0";
+	char * str1 = "HTTP1.1";
+	char * str2 = "HTTP1.0";
 
-	LST_String *token1 = lst_string_new(token1, 1, strlen(token1));
-	LST_String *token2 = lst_string_new(token2, 1, strlen(token2));
+	LST_String *token1 = lst_string_new(str1, 1, strlen(str1));
+	LST_String *token2 = lst_string_new(str2, 1, strlen(str2));
 
 	LST_StringSet *tokens = lst_stringset_new();
 	lst_stringset_add(tokens, token1);
 	lst_stringset_add(tokens, token2);
 	
+
+	/*
 	convertion_data_set_t * data = convertion_data_set_new();	
 
 	lst_stringset_foreach(flows, convert_flow_cb, data);	
 
-
 	convertion_data_set_free(data);
+	*/
+	
+	
+	char * merge = string_replace("This is just for test, for testing.", "test", "aaaa");
 
-
+	printf("%s\n", merge);
 	return 0;
 }
